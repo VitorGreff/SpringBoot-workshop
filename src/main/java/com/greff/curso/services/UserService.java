6c353883a07fb7d5f3554f63562ea038a47fdf2e
VitorGreff <vitorgreffdias@gmail.com>
2023-08-19 00:53:37 -0300

    get by id implemented

---
+++ src/main/java/com/greff/curso/services/UserService.java
@@ -0,0 +1,26 @@
+package com.greff.curso.services;
+
+import com.greff.curso.entities.User;
+import com.greff.curso.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll(){
+        return repository.findAll();
+    }
+
+    public User findById(Long id){
+        Optional<User> obj =  repository.findById(id);
+        return obj.get();
+    }
+
+}
