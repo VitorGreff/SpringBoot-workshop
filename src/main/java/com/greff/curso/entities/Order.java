58e3abceedaaf904feca4dcc9bdef4b82a991b9d
VitorGreff <vitorgreffdias@gmail.com>
2023-08-19 20:22:19 -0300

    Enum implemented

--- src/main/java/com/greff/curso/entities/Order.java
+++ src/main/java/com/greff/curso/entities/Order.java
@@ -1,6 +1,7 @@
 package com.greff.curso.entities;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.greff.curso.entities.enums.OrderStatus;
 import jakarta.persistence.*;
 
 import java.time.Instant;
@@ -19,11 +20,14 @@ public class Order {
     private User client;
 
     public Order(){}
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
     }
+
+    private Integer orderStatus;
 
     public Long getId() {
         return this.id;
@@ -47,6 +51,15 @@ public class Order {
 
     public void setClient(User client) {
         this.client = client;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if(orderStatus!=null)
+            this.orderStatus = orderStatus.getCode();
     }
 
     @Override
